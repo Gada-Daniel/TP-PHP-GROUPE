07e31a0e5c00f9abb9bf12c90043e39d77959eb5
Augutin-Erceville <a.erceville@lprs.fr>
2024-11-29 12:04:08 +0100

    Update(connexion)-Modification de la page de connexion

---
+++ Connexion.php
@@ -0,0 +1,53 @@
+<?php
+$bdd = new PDO('mysql:host=localhost;dbname=tp_crud;charset=utf8', 'root', '');
+
+session_start();
+
+if (isset($_POST['envoyer'])) {
+    $email = htmlspecialchars($_POST['email']);
+    $password = $_POST['password'];
+
+    if (!empty($email) && !empty($password)) {
+        $req = $bdd->prepare('SELECT id_user, email, password FROM utilisateur WHERE email = :email');
+        $req->execute(['email' => $email]);
+        $user = $req->fetch();
+
+        if ($user && password_verify($password, $user['password'])) {
+            $_SESSION['id_user'] = $user['id_user'];
+            $_SESSION['email'] = $user['email'];
+            header('location:index.php');
+            exit();
+        } else {$message = "Identifiants incorrects. Veuillez réessayer.";}
+    } else {$message = "Tous les champs doivent être remplis.";}
+}
+?>
+
+<!DOCTYPE html>
+<html lang="FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<div class="container">
+    <h1>Connexion</h1>
+    <form action="" method="post">
+        <label for="email">Adresse mail</label><br>
+        <input type="email" name="email" id="email" required/><br>
+
+        <label for="password">Mot de passe</label><br>
+        <input type="password" name="password" id="password" required/><br><br>
+
+        <input type="submit" name="envoyer" id="envoyer" value="Se connecter"/>
+    </form>
+
+    <?php
+    if (isset($message)) :
+        ?>
+        <?= $message ?><p></p>
+    <?php endif; ?>
+</div>
+</body>
+</html>
